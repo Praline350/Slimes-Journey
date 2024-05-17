f2b474817dcf12afccf3fe8fc95aa3021b89baf5
edwin thierry <edwin.thierry350@gmail.com>
2024-05-17 13:12:31 +0200

    Test pour chaque models(en cours)

--- tests/event_test.py
+++ tests/event_test.py
@@ -1,13 +1,25 @@
 import os
 import sys
+import time
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from models.event import Event, PassiveEvent, ActiveEvent
 
 
-event = PassiveEvent()
-data = event.spwan_event()
-data = event.get_event_data(data['name'])
-print(data)
+event = Event()
+passive_event = PassiveEvent()
+active_event = ActiveEvent()
+
+"""random_event = event.spawn_event('passive')
+print(random_event)"""
+active_event.deactivate_event()
+
+"""while True:
+    state = active_event.get_activate_event()
+    print(state)
+    time.sleep(1)
+    if state['state'] == 1:
+        break"""
+
 
 
 
